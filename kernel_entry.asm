70587dfb6d7d9e39e6140c00b5f4277bddf46aac
Zeerakyoungdev <zeerak587@gmail.com>
2026-07-07 19:21:54 -0400

    Change entry point to x86-64 and update instructions

--- kernel_entry.asm
+++ kernel_entry.asm
@@ -1,12 +1,20 @@
-; Atlas32 x86 32-bit protected-mode entry point
+; Atlas64 x86-64 kernel entry point
 
-[bits 32]
+[bits 64]
 
 global _start
 extern kernel_main
 
+section .text
 _start:
+    cli
+    cld
+
+    mov rsp, 0x90000
+    xor rbp, rbp
+
     call kernel_main
 
-hang:
-    jmp hang
+.hang:
+    hlt
+    jmp .hang
